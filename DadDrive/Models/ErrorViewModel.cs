47ed66cf1717f3ef90574663ca43fdc3bcb564db
alanAval <alan@avaltech.com.br>
2020-08-13 11:04:18 -0300

    Create new web app project

---
+++ DadDrive/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DadDrive.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
