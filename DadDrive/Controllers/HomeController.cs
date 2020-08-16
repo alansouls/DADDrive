b8bfea165eec1a41256f5464362b9d30f361b05f
eduardoaval <eduardo@avalengenharia.eng.br>
2020-08-16 20:05:13 -0300

    Select file

--- DadDrive/Controllers/HomeController.cs
+++ DadDrive/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
 using DadDrive.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 
 namespace DadDrive.Controllers
 {
@@ -21,6 +21,29 @@ namespace DadDrive.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return Content("file not selected");
+
+            var path = Path.Combine(
+                        Directory.GetCurrentDirectory(), "wwwroot",
+                        file.FileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return RedirectToAction("Files");
+        }
+
+        public IActionResult List()
+        {
+            return Ok();
         }
 
         public IActionResult Privacy()
