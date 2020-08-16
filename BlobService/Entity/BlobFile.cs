9c19f0202bf7ff6241fb38449de36d5200fb4c7a
alanAval <alan@avaltech.com.br>
2020-08-16 19:49:33 -0300

    Comenta o código e faz programa teste para atividade

--- BlobService/Entity/BlobFile.cs
+++ BlobService/Entity/BlobFile.cs
@@ -5,11 +5,26 @@ using System.Text;
 
 namespace BlobServices.Entity
 {
+    /// <summary>
+    /// Estrutura para guardar informações de um arquivo salvo no Blob.
+    /// </summary>
     public class BlobFile
     {
+        /// <summary>
+        /// Nome completo do arquivo: pasta/arquivo
+        /// </summary>
         public string FullName { get; }
+        /// <summary>
+        /// Nome da pasta do arquivo, ou caminho de pastas: pasta ou pasta1/pasta2
+        /// </summary>
         public string Folder { get; set; }
+        /// <summary>
+        /// Nome apenas do arquivo: arquivo.bin
+        /// </summary>
         public string Name { get;}
+        /// <summary>
+        /// Uri para recuperação do arquivo
+        /// </summary>
         public Uri Uri { get;}
 
         public BlobFile(Uri blobUri)
--- BlobService/Service/BlobService.cs
+++ BlobService/Service/BlobService.cs
@@ -13,6 +13,11 @@ namespace BlobServices.Services
     {
         private readonly CloudBlobClient _blobClient;
         private CloudBlobContainer _activeContainer;
+
+        /// <summary>
+        /// Recebe a string de conexão da conta de armazenamento do azure e inicia um cliente blob.
+        /// </summary>
+        /// <param name="connectionString"></param>
         public BlobService(string connectionString)
         {
             var cloudStorageAccount = CloudStorageAccount.Parse(connectionString);
@@ -20,6 +25,10 @@ namespace BlobServices.Services
             _blobClient = cloudStorageAccount.CreateCloudBlobClient();
         }
 
+        /// <summary>
+        /// Recupera a lista de arquivos como objeto BlobFile do container blob ativo
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<BlobFile> ListFiles()
         {
             var blobList = _activeContainer.ListBlobs();
@@ -33,9 +42,13 @@ namespace BlobServices.Services
             return result;
         }
 
+        /// <summary>
+        /// Retorna uma string listando todos os arquivos do container blob ativo.
+        /// </summary>
+        /// <returns></returns>
         public string ListFilesString()
         {
-            var blobList = _activeContainer.ListBlobs();
+            var blobList = _activeContainer.ListBlobs(useFlatBlobListing: true);
 
             string resultStr = "";
             foreach (var blob in blobList)
@@ -51,6 +64,11 @@ namespace BlobServices.Services
             return resultStr;
         }
 
+        /// <summary>
+        /// Recupera o arquivo em um byte array atráves do seu caminho.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         public byte[] RetrieveFile(string path)
         {
             var blobBlock = _activeContainer.GetBlockBlobReference(path);
@@ -63,12 +81,21 @@ namespace BlobServices.Services
             return file;
         }
 
+        /// <summary>
+        /// Ativa um blob container com o nome passado no parâmetro container.
+        /// </summary>
+        /// <param name="container"></param>
         public void SelectBlobContainer(string container)
         {
             _activeContainer = _blobClient.GetContainerReference(container);
             _activeContainer.CreateIfNotExists(BlobContainerPublicAccessType.Blob, null, null);
         }
 
+        /// <summary>
+        /// Faz upload de um arquivo esalva ele com o nome especificado em fileName.
+        /// </summary>
+        /// <param name="file">Byte Array do arquivo.</param>
+        /// <param name="fileName">Nome do arquivo para recuperar depois.</param>
         public void UploadFile(byte[] file, string fileName)
         {
             var blobBlock = _activeContainer.GetBlockBlobReference(fileName);
